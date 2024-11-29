8d1348dca9c764af87b7b4d2502542f245c072d7
Sefleris <p.sefleris@gmail.com>
2024-11-29 09:17:36 +0200

    Update

--- src/components/footer/footerTemplate.js
+++ src/components/footer/footerTemplate.js
@@ -1,7 +1,7 @@
 import './_footer.scss'
 const footerTemplate = ()=>{
     return `
-        <footer class="align-items-end">
+        <footer class="mb-auto">
             <div class="container">
                 <div class="row row-cols-1 row-cols-md-3">
                     <div class="col">
--- src/components/search/searchHeaderTemplate.js
+++ src/components/search/searchHeaderTemplate.js
@@ -2,7 +2,7 @@ function searchHeader (){
     return `
         <div class="mt-5">
             <h1>Discover recipe</h1>
-            <form id="mealSearch" role="search" class="input-group mb-3">
+            <form id="mealSearch" role="search" class="input-group mb-3 row row-cols-1 row-cols-lg-3  g-sm-3">
                 <select name='apiMethod' class="api_method form-select input-group-prepend">
                     <option value="name" selected>Name:</option>
                     <option value="id">Id:</option>
@@ -11,7 +11,7 @@ function searchHeader (){
                     <option value="area">Area:</option>
                     <option value="ingredient">Main ingredient:</option>
                 </select>
-                <input type="text" list=""  class="api_term form-control w-50" placeholder="Search for recipe">
+                <input type="text" list=''  class="api_term form-control w-50" placeholder="Search for recipe">
                 <datalist id="list-ingredient"></datalist>
                 <datalist id="list-category"></datalist>
                 <datalist id="list-area"></datalist>
