7e193ef8fd0e68b7e5fb491bdb35e095e2298784
Sefleris <p.sefleris@gmail.com>
2024-11-29 08:08:21 +0200

    Initial gh pages

--- src/components/search/optionTemplate.js
+++
@@ -1,7 +0,0 @@
-function optionTemplate (optionValue, optionText=''){
-    return`
-        <option value=${optionValue}>${optionText}</option>
-    `
-}
-
-export default optionTemplate;
--- src/settings.js
+++
@@ -1,4 +0,0 @@
-const apiKey = '1';
-const apiUrl = 'https://www.themealdb.com/api/json/v1/'
-
-export {apiKey, apiUrl};
